a207fb4f88974af4dd2d390c98d3b5bfd49873c8
Pavani Kuppili <41535888+pavanikuppili@users.noreply.github.com>
2025-02-15 20:02:44 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -83,7 +83,7 @@ lan1.setVlanTag(2711)
 lan2.setVlanTag(2712)
 
 nodeList = params.nodes.split(',')
-i = 0
+n_idx = 0
 for nodeName in nodeList:
     host = request.RawPC(nodeName)
     # UMass cluster
@@ -113,29 +113,31 @@ for nodeName in nodeList:
     fpga.setUseTypeDefaultImage()
 
     # Secret sauce.
-    fpga.SubNodeOf(host)
+    
 
-    if i==0:
+    if n_idx == 0:
+        fpga.SubNodeOf(host)
         host_iface1 = host.addInterface()
         host_iface1.component_id = "eth2"
-        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(i+30), "255.255.255.0")) 
+        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+30), "255.255.255.0")) 
         lan1.addInterface(host_iface1)
-    if i==1:
+        fpga_iface1 = fpga.addInterface()
+        fpga_iface1.component_id = "eth0"
+        fpga_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+10), "255.255.255.0"))
+        fpga_iface2 = fpga.addInterface()
+        fpga_iface2.component_id = "eth1"
+        fpga_iface2.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+20), "255.255.255.0"))
+        lan2.addInterface(fpga_iface1)
+        lan2.addInterface(fpga_iface2)
+    else:
         host_iface1 = host.addInterface()
         host_iface1.component_id = "eth2"
-        host_iface1.addAddress(pg.IPv4Address("192.168.50." + str(i+30), "255.255.255.0"))
+        host_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+30), "255.255.255.0")) 
         lan2.addInterface(host_iface1)
-
-    fpga_iface1 = fpga.addInterface()
-    fpga_iface1.component_id = "eth0"
-    fpga_iface1.addAddress(pg.IPv4Address("192.168.50." + str(i+10), "255.255.255.0"))
-    fpga_iface2 = fpga.addInterface()
-    fpga_iface2.component_id = "eth1"
-    fpga_iface2.addAddress(pg.IPv4Address("192.168.50." + str(i+20), "255.255.255.0"))
-    lan2.addInterface(fpga_iface1)
-    lan2.addInterface(fpga_iface2) 
+      
   
-    i+=1
+  
+n_idx = n_idx + 1
 
 # Print Request RSpec
 pc.printRequestRSpec(request)

555402e2bb1e221e58b3e5cb254245fddd0eda53
Pavani Kuppili <41535888+pavanikuppili@users.noreply.github.com>
2025-03-24 12:49:54 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -142,8 +142,8 @@ for nodeName in nodeList:
     else:
         host_iface1 = host.addInterface()
         host_iface2 = host.addInterface()
-        host_iface1.component_id = "eth1"
-        host_iface2.component_id = "eth2"
+        host_iface1.component_id = "eth0"
+        host_iface2.component_id = "eth1"
         host_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+30), "255.255.255.0"))
         host_iface2.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+30), "255.255.255.0"))
         lan2.addInterface(host_iface1)

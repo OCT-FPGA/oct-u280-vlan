20b5fbdc74edc4df1bcb7d5f6b76d8b52d6989ea
Pavani Kuppili <41535888+pavanikuppili@users.noreply.github.com>
2025-06-15 19:28:20 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -140,29 +140,14 @@ for nodeName in nodeList:
         lan2.addInterface(fpga_iface1)
         lan2.addInterface(fpga_iface2)
     else:
-        # host_iface1 = host.addInterface()
-        # host_iface1.component_id = "eth3"
-        # host_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+30), "255.255.255.0"))
-        fpga_iface1 = fpga.addInterface()
-        fpga_iface1.component_id = "eth0"
-        lan1.addInterface(fpga_iface1)
-        lan2.addInterface(fpga_iface1)
-        fpga_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+10), "255.255.255.0"))
-        fpga_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+10), "255.255.255.0"))
-      
-        fpga_iface2 = fpga.addInterface()
-        fpga_iface2.component_id = "eth1"
-        lan1.addInterface(fpga_iface2)
-        lan2.addInterface(fpga_iface2)
-        fpga_iface2.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+20), "255.255.255.0"))
-        fpga_iface2.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+20), "255.255.255.0"))
-        # lan2.addInterface(host_iface1)
-        
-        
-        
-        
-        
-        # host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+30), "255.255.255.0"))
+        host_iface1 = host.addInterface()
+        # host_iface2 = host.addInterface()
+        host_iface1.component_id = "eth3"
+        # host_iface2.component_id = "eth3"
+        lan2.addInterface(host_iface1)
+        lan1.addInterface(host_iface1)
+        host_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+30), "255.255.255.0"))
+        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+30), "255.255.255.0"))
         
     
     # lan1.link_multiplexing = True

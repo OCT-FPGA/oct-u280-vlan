320ad485f47426b36bb9066aa4ddb09b8ec6e6aa
Pavani Kuppili <41535888+pavanikuppili@users.noreply.github.com>
2025-06-16 01:43:12 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -129,8 +129,12 @@ for nodeName in nodeList:
     if n_idx == 0:
         host_iface1 = host.addInterface()
         host_iface1.component_id = "eth3"
-        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+30), "255.255.255.0")) 
+        lan2.addInterface(host_iface1)
         lan1.addInterface(host_iface1)
+        host_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+30), "255.255.255.0"))
+        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+30), "255.255.255.0"))
+        
+      
         fpga_iface1 = fpga.addInterface()
         fpga_iface1.component_id = "eth0"
         fpga_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+10), "255.255.255.0"))
@@ -141,9 +145,7 @@ for nodeName in nodeList:
         lan2.addInterface(fpga_iface2)
     else:
         host_iface1 = host.addInterface()
-        # host_iface2 = host.addInterface()
         host_iface1.component_id = "eth3"
-        # host_iface2.component_id = "eth3"
         lan2.addInterface(host_iface1)
         lan1.addInterface(host_iface1)
         host_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+30), "255.255.255.0"))

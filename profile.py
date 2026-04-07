d61cf3606926fddfe0bca5a8b3a29e75dcbc0980
Pavani Kuppili <41535888+pavanikuppili@users.noreply.github.com>
2026-04-07 09:22:28 -0600

    Comment out interface address assignments
    Comment out IP address assignments for interfaces in profile.py.

--- profile.py
+++ profile.py
@@ -125,14 +125,14 @@ for nodeName in nodeList:
     if n_idx == 0:
         host_iface1 = host.addInterface()
         host_iface1.component_id = "eth2"
-        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+30), "255.255.255.0")) 
+        # host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(n_idx+30), "255.255.255.0")) 
         lan1.addInterface(host_iface1)
         fpga_iface1 = fpga.addInterface()
         fpga_iface1.component_id = "eth0"
-        fpga_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+10), "255.255.255.0"))
+        # fpga_iface1.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+10), "255.255.255.0"))
         fpga_iface2 = fpga.addInterface()
         fpga_iface2.component_id = "eth1"
-        fpga_iface2.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+20), "255.255.255.0"))
+        # fpga_iface2.addAddress(pg.IPv4Address("192.168.50." + str(n_idx+20), "255.255.255.0"))
         lan2.addInterface(fpga_iface1)
         lan2.addInterface(fpga_iface2)
     else:
